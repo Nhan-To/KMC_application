4d5ee619b71ccc34f2f550c3bdfa49210e7add09
ToChauHaoNhan <tochauhaonhan0812@gmail.com>
2024-03-29 17:59:01 +0700

    KMC v1.0

---
+++ kmc/src/renderer/src/components/Sell.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect } from 'react';
+import SearchBar from './SearchBar';
+
+function Sell() {
+    return (
+        <>
+            <SearchBar/>
+        </>
+    );
+}
+
+export default Sell;
